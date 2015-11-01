5392eb727bf731a2cc3d0c03eb85f5856d71d48a
Tereta <tereta@mail.ua>
2015-11-01 10:44:36 +0200

    Update repository

---
+++ app/code/local/W3Site/Forum/sql/w3site_forum_setup/mysql4-upgrade-0.0.3-0.0.4.php
@@ -0,0 +1,58 @@
+<?php
+/*******************************************************************************
+ * ██╗    ██╗██████╗ ███████╗██╗████████╗███████╗    ██████╗ ██████╗  ██████╗  *
+ * ██║    ██║╚════██╗██╔════╝██║╚══██╔══╝██╔════╝   ██╔═══██╗██╔══██╗██╔════╝  *
+ * ██║ █╗ ██║ █████╔╝███████╗██║   ██║   █████╗     ██║   ██║██████╔╝██║  ███╗ *
+ * ██║███╗██║ ╚═══██╗╚════██║██║   ██║   ██╔══╝     ██║   ██║██╔══██╗██║   ██║ *
+ * ╚███╔███╔╝██████╔╝███████║██║   ██║   ███████╗██╗╚██████╔╝██║  ██║╚██████╔╝ *
+ *  ╚══╝╚══╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝   ╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝  *
+ *                                                                             *
+ **************************** http://w3site.org/ *******************************
+ **************************Author: Tereta Alexander ****************************
+ * 
+ * W3Site_Forum by Tereta Alexander (w3site.org)
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to tereta@mail.ua so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade extension to newer
+ * versions in the future. If you wish to customize extension for your
+ * needs please refer to http://www.w3site.org for more information.
+ *
+ * @category    W3Site
+ * @package     W3Site_Forum
+ * @copyright   Copyright (c) 2015-2016 Tereta Alexander. (http://www.w3site.org)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Forum update script
+ *
+ * @category   W3Site
+ * @package    W3Site_Forum
+ * @author     Teretea Alexander <tereta@mail.ua>
+ */
+
+$this->startSetup();
+
+$this->getConnection()->changeColumn($this->getTable('w3site_forum/subject'),
+    'order',
+    'created',
+    Varien_Db_Ddl_Table::TYPE_DATETIME
+);
+
+$this->getConnection()->changeColumn($this->getTable('w3site_forum/message'),
+    'order',
+    'created',
+    Varien_Db_Ddl_Table::TYPE_DATETIME
+);
+
+$this->endSetup();
---
+++ app/code/local/W3Site/Forum/sql/w3site_forum_setup/mysql4-upgrade-0.0.4-0.0.5.php
@@ -0,0 +1,62 @@
+<?php
+/*******************************************************************************
+ * ██╗    ██╗██████╗ ███████╗██╗████████╗███████╗    ██████╗ ██████╗  ██████╗  *
+ * ██║    ██║╚════██╗██╔════╝██║╚══██╔══╝██╔════╝   ██╔═══██╗██╔══██╗██╔════╝  *
+ * ██║ █╗ ██║ █████╔╝███████╗██║   ██║   █████╗     ██║   ██║██████╔╝██║  ███╗ *
+ * ██║███╗██║ ╚═══██╗╚════██║██║   ██║   ██╔══╝     ██║   ██║██╔══██╗██║   ██║ *
+ * ╚███╔███╔╝██████╔╝███████║██║   ██║   ███████╗██╗╚██████╔╝██║  ██║╚██████╔╝ *
+ *  ╚══╝╚══╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝   ╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝  *
+ *                                                                             *
+ **************************** http://w3site.org/ *******************************
+ **************************Author: Tereta Alexander ****************************
+ * 
+ * W3Site_Forum by Tereta Alexander (w3site.org)
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to tereta@mail.ua so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade extension to newer
+ * versions in the future. If you wish to customize extension for your
+ * needs please refer to http://www.w3site.org for more information.
+ *
+ * @category    W3Site
+ * @package     W3Site_Forum
+ * @copyright   Copyright (c) 2015-2016 Tereta Alexander. (http://www.w3site.org)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Forum update script
+ *
+ * @category   W3Site
+ * @package    W3Site_Forum
+ * @author     Teretea Alexander <tereta@mail.ua>
+ */
+
+$this->startSetup();
+
+$this->getConnection()->addColumn($this->getTable('w3site_forum/subject'),
+    'updated',
+    array(
+        'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
+        'comment' => 'Updated'
+    )
+);
+
+$this->getConnection()->addColumn($this->getTable('w3site_forum/message'),
+    'updated',
+    array(
+        'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
+        'comment' => 'Updated'
+    )
+);
+
+$this->endSetup();
